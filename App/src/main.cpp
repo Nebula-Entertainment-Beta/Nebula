2d1781866e9c83a3a0db2a7c394a98b1536e32cd
Ibrahim12610 <faisalibrahim610@gmail.com>
2026-02-15 01:23:20 -0600

    fixed window bug

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -9,12 +9,15 @@ int main()
     std::cout << "Hello, world!" << std::endl;
     std::cout << "The magic number is " << magicNumber << std::endl;
 
+    // Create a window using the glfw library
     Nebula::Window window("Nebula", 800, 600);
+    //making the window visible
     while (!window.destroy())
     {
         window.update();
     }
 
+    // Destroy the window and clean up resources
     window.destroy();
 
     return 0;
